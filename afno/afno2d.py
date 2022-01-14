fb383561eb3f0d9109abb046dbaab68b84e149a1
John Guibas <jtguibas@gmail.com>
2022-01-13 23:04:47 -0800

    Fixed small bug on afno2d

--- afno/afno2d.py
+++ afno/afno2d.py
@@ -1,3 +1,4 @@
+import math
 import torch
 import torch.fft
 import torch.nn as nn
@@ -80,5 +81,6 @@ class AFNO2D(nn.Module):
         x = torch.view_as_complex(x)
         x = x.reshape(B, x.shape[1], x.shape[2], C)
         x = torch.fft.irfft2(x, s=(H, W), dim=(1, 2), norm="ortho")
+        x = x.reshape(B, N, C)
         x = x.type(dtype)
         return x + bias
